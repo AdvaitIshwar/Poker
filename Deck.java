749f4871d9f83c1e00939b6ce143af5f080c44e9
AdvaitIshwar <45025807+AdvaitIshwar@users.noreply.github.com>
2019-07-01 13:14:08 -0400

    Add files via upload

--- Deck.java
+++ Deck.java
@@ -10,7 +10,37 @@ public class Deck {
 			}
 		}
 	}
-	public void shuffle() {
+	public void dealPlayers(ArrayList<Player> players) {
 		Collections.shuffle(deck);
+		for(int round=0; round<2; round++) {
+			for(int i=0; i<players.size(); i++) {
+				Player get = players.get(i);
+				get.add(deck.get(0));
+				deck.remove(0);
+			}
+		}
+	}
+	public ArrayList<Card> dealFlop() {
+		ArrayList<Card> table = new ArrayList<Card>();
+		//Burn
+		deck.remove(0);
+		//Flop
+		table.add(deck.get(0));
+		table.add(deck.get(1));
+		table.add(deck.get(2));
+		
+		deck.remove(0);
+		deck.remove(0);
+		deck.remove(0);
+		return table;
+	}
+	public ArrayList<Card> dealTurnAndRiver(ArrayList<Card>table) {
+		//Burn
+		deck.remove(0);
+		//Turn
+		table.add(deck.get(0));
+		
+		deck.remove(0);
+		return table;
 	}
 }
