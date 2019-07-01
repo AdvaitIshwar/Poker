f01900d243156a25999e20f67b9f49e8874d8810
AdvaitIshwar <45025807+AdvaitIshwar@users.noreply.github.com>
2019-07-01 16:45:39 -0400

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,11 +1,9 @@
-
-
 public class Card{
 	int rank;
 	int suit;
 	private final static String[] suitNames = {"s", "h", "c", "d"};
-	private final static String[] valueNames = {"Unused", "A", "2", "3", "4", 
-		"5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	private final static String[] valueNames = {"Unused", "2", "3", "4", 
+		"5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 	public Card(int rank, int suit) {
 		this.suit = suit;
 		this.rank = rank;
@@ -20,16 +18,21 @@ public class Card{
 
 		String retValue;
 		retValue = suitNames[suit];
-		if (rank <= 10)
+		if (rank <= 9)
 			retValue += rank;
-		else if (rank == 11)
+		else if (rank == 10)
 			retValue += "j";
-		else if (rank == 12)
+		else if (rank == 11)
 			retValue += "q";
-		else if (rank == 13)
+		else if (rank == 12)
 			retValue += "k";
+		else if (rank == 13)
+			retValue += "1";
 		else 
 			retValue += "Unknown!";
 		return "images/" + retValue + ".gif";	
+	}
+	public String toString() {
+		return valueNames[rank] + " of " + suitNames[suit];
 	}
 }
